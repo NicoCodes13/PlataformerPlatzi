51d3074c9fd7d2972954d945491936a083429f3b
Nicolas Castañeda <nickcar13@gmail.com>
2020-02-13 13:45:58 -0500

    Animaciones e inicio de particulas

---
+++ Assets/Script/EnemyBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    
+    Rigidbody2D enemyRB;
+    SpriteRenderer enemySpriteRend;
+    Animator enemyAnim;
+    float timeBeforeChange;
+    public float delay = 0.5f;
+    public float speed = 0.3f;
+
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+        enemySpriteRend = GetComponent<SpriteRenderer>();
+        enemyAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRB.velocity = Vector2.right * speed;
+
+        if(speed < 0)
+            enemySpriteRend.flipX = true;
+        else if(speed > 0)
+            enemySpriteRend.flipX =false;
+
+        if(timeBeforeChange < Time.time)
+        {
+            speed *= -1;
+            timeBeforeChange = Time.time + delay;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if(transform.position.y + 0.3f < other.transform.position.y)
+            {
+                enemyAnim.SetBool("isDead", true);
+                
+            }
+        }
+    }
+
+    public void DesableEnemy()
+    {
+        gameObject.SetActive(false);
+        //* Destroy(gameObject); eliminando de la herarquia 
+    }
+
+}
